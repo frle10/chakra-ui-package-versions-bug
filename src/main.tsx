0909b9ec0bc4791dc9d9b6a0146d4b7ba3b49865
Ivan Skorupan <ivan.skorupan@true-north.hr>
2022-07-25 15:06:34 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+import 'moment/dist/locale/hr';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
